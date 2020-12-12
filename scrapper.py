d25f4daccad54fa22e1a24aa805a238bbfa287dc
P Radha Krishna <pradha.krishna.cse17@itbhu.ac.in>
2020-12-12 21:51:12 +0530

    Added roll numbers

--- scrapper.py
+++ scrapper.py
@@ -3,20 +3,27 @@ import json
 import os
 
 
-class IETDAVVradeSpider(scrapy.Spider):
+class IETDAVVGradeSpider(scrapy.Spider):
     name = "Gradesheet"
 
     def start_requests(self):
         # enter the build url here
-        url = ''
-        yield scrapy.Request(url=url, callback=self.parseGradesheet)
+        codes = ['C','V','I','E','T','M']
+        batches = ['0','1']
+        nums = [i for i in range(1,100)]
+        years = [('19','2'),('18','4'),('17','6')]
+        rolls = ['{}{}{}{}{:0>2}'.format(y[0],c,y[1],b,n) for c in codes for b in batches for n in nums for y in years]
+        urlf = "http://results.ietdavv.edu.in/DisplayStudentResult?rollno={}&typeOfStudent=Regular"
+        for roll in rolls:
+            url = urlf.format(roll)
+            yield scrapy.Request(url=url, callback=self.parseGradesheet)
 
     def clean(self, s):
         # cleaning the page
 
     def parseGradesheet(self, response):
         page = response.url
-        if page.split('/')[-1] == 'gradesheet.html':
+        if True:
 
         else:
             self.log('Wierd site %s', response.url)
